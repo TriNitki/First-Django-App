98c2ef3aa9bb855a89389df9887930496b21aab9
Danil Afanasev <fsb2004pro@gmail.com>
2022-10-17 23:08:17 +0700

    Create new_entry.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'), # Page with a list of all topics
     path('topics/<int:topic_id>/', views.topic, name='topic'), # Page with an addition information about topic
     path('new_topic/', views.new_topic, name='new_topic'), # Page for creating new topics
-
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'), # Page for creating new entries 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     '''Home page of Learning Log'''
@@ -35,3 +35,22 @@ def new_topic(request):
     # Displays blanck or unvalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    '''Adds new entry about specific topic'''
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Data isn't sent; Creates blanck form
+        form = EntryForm()
+    else:
+        # Displays POST data; Processes data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    
+    # Displays blanck or unvalid form
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
