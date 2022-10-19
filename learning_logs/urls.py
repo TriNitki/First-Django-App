6d97546d10af62f1ec2d623af95796112bcd3220
Danil Afanasev <fsb2004pro@gmail.com>
2022-10-19 19:19:34 +0700

    Edit entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'), # Page with an addition information about topic
     path('new_topic/', views.new_topic, name='new_topic'), # Page for creating new topics
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'), # Page for creating new entries 
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'), # Page for editing entries
 ]
