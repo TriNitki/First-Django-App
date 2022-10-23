3933f42db7a79aebd12df7cf40f2664e989d1a70
Danil Afanasev <fsb2004pro@gmail.com>
2022-10-23 14:22:11 +0700

    registration function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    '''Registrates new user'''
+    if request.method != 'POST':
+        # Creates blank registration form
+        form = UserCreationForm()
+    else:
+        # Processes filled form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Sign in and redirect on home page 
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    
+    # Create blanck and unvalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
+
+
